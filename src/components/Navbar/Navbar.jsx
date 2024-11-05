ec2e8515b3bfd43cee0b8f95d8c9ef36bbefb208
Amrin91 <100765562+Amrin91@users.noreply.github.com>
2024-11-05 18:52:20 +0600

    first commit

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,78 @@
+import { useContext, useState, useEffect } from "react";
+import { StoreContext } from "../../context/StoreContext";
+import "./Navbar.css";
+import { assets } from "../../assets/assets";
+import { Link } from "react-router-dom";
+
+const Navbar = ({ setShowLogin }) => {
+  const { getTotalQuantity } = useContext(StoreContext);
+  const totalQuantity = getTotalQuantity();
+
+  const [menu, setMenu] = useState("home");
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setScrolled(window.scrollY > 0);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  return (
+    <div className={`navbar ${scrolled ? "scrolled" : ""}`}>
+      <Link to="/">
+        <h2 className="logo">Cravings</h2>
+      </Link>
+      <ul className="navbar-menu">
+        <Link
+          to="/"
+          onClick={() => setMenu("home")}
+          className={menu === "home" ? "active" : ""}
+        >
+          Home
+        </Link>
+        <a
+          href="#explore-menu"
+          onClick={() => setMenu("menu")}
+          className={menu === "menu" ? "active" : ""}
+        >
+          Menu
+        </a>
+        <a
+          href="#app-download"
+          onClick={() => setMenu("mobile-app")}
+          className={menu === "mobile-app" ? "active" : ""}
+        >
+          Mobile App
+        </a>
+        <a
+          href="#footer"
+          onClick={() => setMenu("contact-us")}
+          className={menu === "contact-us" ? "active" : ""}
+        >
+          Contact Us
+        </a>
+      </ul>
+      
+      <div className="navbar-right">
+        
+        <div className="navbar-basket-icon">
+          <Link to="/cart">
+            <img src={assets.basket_icon} alt="basket_icon" />
+          </Link>
+          <div className={totalQuantity === 0 ? "dotHidden" : "dot"}>
+            <p>{totalQuantity}</p>
+          </div>
+        </div>
+        <button onClick={() => setShowLogin(true)}>Sign in</button>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
